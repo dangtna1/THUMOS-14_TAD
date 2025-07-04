0d05249465f4b3cad483297797080481dcdd8bc8
Dang Vu <tamquattnb@gmail.com>
2025-07-04 15:56:15 +0100

    feat: add convert_to_seconds function for segment time conversion

--- tad/models/utils/post_processing/__init__.py
+++ tad/models/utils/post_processing/__init__.py
@@ -1,3 +1,3 @@
-from .utils import load_predictions, save_predictions
+from .utils import load_predictions, save_predictions, convert_to_seconds
 
-__all__ = [load_predictions, save_predictions]
+__all__ = [load_predictions, save_predictions, convert_to_seconds]
--- tad/models/utils/post_processing/utils.py
+++ tad/models/utils/post_processing/utils.py
@@ -44,3 +44,25 @@ def save_predictions(predictions, metas, folder):
         prediction = [data[idx] for data in predictions]
         with open(file_path, "wb") as outfile:
             pickle.dump(prediction, outfile, pickle.HIGHEST_PROTOCOL)
+
+
+def convert_to_seconds(segments, meta):
+    if meta["fps"] == -1:  # resize setting, like in anet / hacs
+        segments = segments / meta["resize_length"] * meta["duration"]
+    else:  # sliding window / padding setting, like in thumos / ego4d
+        snippet_stride = meta["snippet_stride"]
+        offset_frames = meta["offset_frames"]
+        window_start_frame = (
+            meta["window_start_frame"] if "window_start_frame" in meta.keys() else 0
+        )
+        segments = (
+            segments * snippet_stride + window_start_frame + offset_frames
+        ) / meta["fps"]
+
+    # truncate all boundaries within [0, duration]
+    if segments.shape[0] > 0:
+        segments[segments <= 0.0] *= 0.0
+        segments[segments >= meta["duration"]] = (
+            segments[segments >= meta["duration"]] * 0.0 + meta["duration"]
+        )
+    return segments
