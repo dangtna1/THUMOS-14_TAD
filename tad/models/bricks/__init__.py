30e5ce79d31b701d2f1a5e3c9094c1c35c83e162
Dang Vu <tamquattnb@gmail.com>
2025-07-06 11:35:13 +0100

    feat: add ConvModule and some other reusable modules in brick layer

--- tad/models/bricks/__init__.py
+++ tad/models/bricks/__init__.py
@@ -1,4 +1,20 @@
-from .misc import Scale
-from .transformer import AffineDropPath
+from .conv import ConvModule
 
-__all__ = [Scale, AffineDropPath]
+# from .gcnext import GCNeXt
+from .misc import Scale
+from .transformer import TransformerBlock, AffineDropPath
+
+# from .bottleneck import ConvNeXtV1Block, ConvNeXtV2Block, ConvFormerBlock
+# from .sgp import SGPBlock
+
+__all__ = [
+    "ConvModule",
+    # "GCNeXt",
+    "Scale",
+    "TransformerBlock",
+    "AffineDropPath",
+    # "SGPBlock",
+    # "ConvNeXtV1Block",
+    # "ConvNeXtV2Block",
+    # "ConvFormerBlock",
+]
