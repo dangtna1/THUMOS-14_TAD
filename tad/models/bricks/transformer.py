716f223950211eec6884d4dcf351c8dac3bca6a7
Dang Vu <tamquattnb@gmail.com>
2025-07-09 23:04:11 +0100

    feat: add DropPath class and drop_path function for stochastic depth implementation

--- tad/models/bricks/transformer.py
+++ tad/models/bricks/transformer.py
@@ -605,6 +605,35 @@ class LocalMaskedMHCA(nn.Module):
         return out, qx_mask
 
 
+# The follow code is modified from
+# https://github.com/facebookresearch/SlowFast/blob/master/slowfast/models/common.py
+def drop_path(x, drop_prob=0.0, training=False):
+    """
+    Stochastic Depth per sample.
+    """
+    if drop_prob == 0.0 or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (
+        x.ndim - 1
+    )  # work with diff dim tensors, not just 2D ConvNets
+    mask = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
+    mask.floor_()  # binarize
+    output = x.div(keep_prob) * mask
+    return output
+
+
+class DropPath(nn.Module):
+    """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks)."""
+
+    def __init__(self, drop_prob=None):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training)
+
+
 class AffineDropPath(nn.Module):
     """
     Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks) with a per channel scaling factor (and zero init)
