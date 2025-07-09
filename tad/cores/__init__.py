c0c01945cdeb3c077ef74358dbf51ffb006f0bcd
Dang Vu <tamquattnb@gmail.com>
2025-07-09 23:09:28 +0100

    feat: implement training and validation functions, optimizer, and scheduler for model training

---
+++ tad/cores/__init__.py
@@ -0,0 +1,11 @@
+from .train_engine import train_one_epoch, val_one_epoch
+from .optimizer import build_optimizer
+from .scheduler import build_scheduler
+
+__all__ = [
+    "train_one_epoch",
+    "val_one_epoch",
+    "eval_one_epoch",
+    "build_optimizer",
+    "build_scheduler",
+]
--- tools/train.py
+++ tools/train.py
@@ -6,14 +6,21 @@ path = os.path.join(os.path.dirname(__file__), "..")
 if path not in sys.path:
     sys.path.insert(0, path)
 
-
 import argparse
-
+import torch
 from mmengine.config import Config, DictAction
 
 from tad.datasets import build_dataset, build_dataloader
-from tad.datasets import transforms  # registers everything under transforms
 from tad.models import build_detector
+from tad.cores import (
+    train_one_epoch,
+    build_optimizer,
+    build_scheduler,
+)
+from tad.utils import (
+    setup_logger,
+    ModelEma,
+)
 
 
 def parse_args():
@@ -46,10 +53,12 @@ def main():
     cfg = Config.fromfile(args.config)
     print(f"Loaded config from {args.config}")
 
-    # # setup logger
-    # logger = setup_logger("Train", save_dir=cfg.work_dir, distributed_rank=args.rank)
-    # logger.info(f"Using torch version: {torch.__version__}, CUDA version: {torch.version.cuda}")
-    # logger.info(f"Config: \n{cfg.pretty_text}")
+    # setup logger
+    logger = setup_logger("Train", save_dir=cfg.work_dir, distributed_rank=0)
+    logger.info(
+        f"Using torch version: {torch.__version__}, CUDA version: {torch.version.cuda}"
+    )
+    logger.info(f"Config: \n{cfg.pretty_text}")
 
     # build dataset
     train_dataset = build_dataset(cfg.dataset.train, default_args=dict(logger=None))
@@ -63,16 +72,110 @@ def main():
     )
 
     # Debug
-    batch = next(iter(train_loader))
-    print(batch.keys())
-    print(batch["inputs"].shape)
-    print(batch["masks"].shape)
-    print(batch["gt_segments"])
-    print(batch["gt_labels"])
-    print(batch["metas"])
+    # batch = next(iter(train_loader))
+    # print(batch.keys())
+    # print(batch["inputs"].shape)
+    # print(batch["masks"].shape)
+    # print(batch["gt_segments"])
+    # print(batch["gt_labels"])
+    # print(batch["metas"])
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # build model
-    model = build_detector(cfg.model)
+    model = build_detector(cfg.model).to(device)
+
+    # Model EMA
+    use_ema = getattr(cfg.solver, "ema", False)
+    if use_ema:
+        model_ema = ModelEma(model)
+    else:
+        model_ema = None
+
+    # AMP: automatic mixed precision
+    use_amp = getattr(cfg.solver, "amp", False)
+    if use_amp:
+        scaler = GradScaler()
+    else:
+        scaler = None
+
+    # build optimizer and scheduler
+    optimizer = build_optimizer(cfg.optimizer, model, logger=None)
+    scheduler, max_epoch = build_scheduler(cfg.scheduler, optimizer, len(train_loader))
+
+    # override the max_epoch
+    # max_epoch = cfg.workflow.get("end_epoch", max_epoch)
+
+    # train the detector
+    logger.info("Training Starts...\n")
+    val_loss_best = 1e6
+    val_start_epoch = cfg.workflow.get("val_start_epoch", 0)
+    for epoch in range(0, 1):
+        # train_loader.sampler.set_epoch(epoch)
+
+        # train for one epoch
+        train_one_epoch(
+            train_loader,
+            model,
+            optimizer,
+            scheduler,
+            epoch,
+            logger=logger,
+            model_ema=model_ema,
+            clip_grad_l2norm=cfg.solver.clip_grad_norm,
+            logging_interval=cfg.workflow.logging_interval,
+            scaler=scaler,
+        )
+
+    #     # save checkpoint
+    #     if (epoch == max_epoch - 1) or (
+    #         (epoch + 1) % cfg.workflow.checkpoint_interval == 0
+    #     ):
+    #         if args.rank == 0:
+    #             save_checkpoint(
+    #                 model, model_ema, optimizer, scheduler, epoch, work_dir=cfg.work_dir
+    #             )
+
+    #     # val for one epoch
+    #     if epoch >= val_start_epoch:
+    #         if (cfg.workflow.val_loss_interval > 0) and (
+    #             (epoch + 1) % cfg.workflow.val_loss_interval == 0
+    #         ):
+    #             val_loss = val_one_epoch(
+    #                 val_loader,
+    #                 model,
+    #                 logger,
+    #                 args.rank,
+    #                 epoch,
+    #                 model_ema=model_ema,
+    #                 use_amp=use_amp,
+    #             )
+
+    #             # save the best checkpoint
+    #             if val_loss < val_loss_best:
+    #                 logger.info(f"New best epoch {epoch}")
+    #                 val_loss_best = val_loss
+    #                 if args.rank == 0:
+    #                     save_best_checkpoint(
+    #                         model, model_ema, epoch, work_dir=cfg.work_dir
+    #                     )
+
+    #     # eval for one epoch
+    #     if epoch >= val_start_epoch:
+    #         if (cfg.workflow.val_eval_interval > 0) and (
+    #             (epoch + 1) % cfg.workflow.val_eval_interval == 0
+    #         ):
+    #             eval_one_epoch(
+    #                 test_loader,
+    #                 model,
+    #                 cfg,
+    #                 logger,
+    #                 args.rank,
+    #                 model_ema=model_ema,
+    #                 use_amp=use_amp,
+    #                 world_size=args.world_size,
+    #                 not_eval=args.not_eval,
+    #             )
 
 
 if __name__ == "__main__":
