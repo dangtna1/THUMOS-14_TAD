e8e04a7f500f9061c3cf7f02429457d999067f34
Dang Vu <tamquattnb@gmail.com>
2025-07-11 23:15:13 +0100

    feat: add classifier module with multiple classifier implementations and update __all__ exports

--- tad/models/utils/post_processing/__init__.py
+++ tad/models/utils/post_processing/__init__.py
@@ -1,3 +1,5 @@
+from .nms.nms import batched_nms
 from .utils import load_predictions, save_predictions, convert_to_seconds
+from .classifier import build_classifier
 
-__all__ = [load_predictions, save_predictions, convert_to_seconds]
+__all__ = ["batched_nms", "load_predictions", "save_predictions", "convert_to_seconds"]
