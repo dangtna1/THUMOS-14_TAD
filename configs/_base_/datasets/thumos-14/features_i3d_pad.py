5d5b19b6fd3234a5d71dbf9de65ae701fe18e020
Dang Vu <tamquattnb@gmail.com>
2025-07-29 18:55:48 +0100

    update file paths

--- configs/_base_/datasets/thumos-14/features_i3d_pad.py
+++ configs/_base_/datasets/thumos-14/features_i3d_pad.py
@@ -1,7 +1,7 @@
 dataset_type = "ThumosPaddingDataset"
-annotation_path = "data/annotations/thumos_14_anno.json"
-class_map = "data/annotations/category_idx.txt"
-data_path = "data/features/i3d_actionformer_stride4_thumos/"
+annotation_path = "data/thumos-14/annotations/thumos_14_anno.json"
+class_map = "data/thumos-14/annotations/category_idx.txt"
+data_path = "data/thumos-14/features/i3d_actionformer_stride4_thumos/"
 block_list = data_path + "missing_files.txt"
 
 trunc_len = 2304
@@ -79,4 +79,11 @@ dataset = dict(
             dict(type="Collect", inputs="feats", keys=["masks"]),
         ],
     ),
+)
+
+evaluation = dict(
+    type="mAP",
+    subset="validation",
+    tiou_thresholds=[0.3, 0.4, 0.5, 0.6, 0.7],
+    ground_truth_filename=annotation_path,
 )
