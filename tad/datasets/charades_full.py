fix: ensure image normalization uses float32 in load_rgb_frames function

tad/datasets/charades_full.py
@@ -32,7 +32,7 @@ def load_rgb_frames(image_dir, ego_id, start, num):
             d = 226.0 - min(w, h)
             sc = 1 + d / min(w, h)
             img = cv2.resize(img, dsize=(0, 0), fx=sc, fy=sc)
-        img = (img / 255.0) * 2 - 1
+        img = (img.astype(np.float32) / 255.0) * 2 - 1
         frames.append(img)
     return np.asarray(frames, dtype=np.float32)
 

tools/extract_features_full.py
@@ -49,7 +49,7 @@ def run(
 
     dataset = Charades(ann_file, data_path, mode, test_transforms, save_dir=save_dir)
     dataloader = torch.utils.data.DataLoader(
-        dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True
+        dataset, batch_size=batch_size, shuffle=True, num_workers=8
     )
 
     # setup the model
@@ -74,22 +74,22 @@ def run(
 
         b, c, t, h, w = inputs.shape  # b = batch = 1
         if t > 1600:
-            features = []
-            for start in range(1, t - 56, 1600):
-                end = min(t - 1, start + 1600 + 56)
-                start = max(1, start - 48)
-                ip = Variable(
-                    torch.from_numpy(inputs.numpy()[:, :, start:end]).cuda(),
-                    volatile=True,
-                )
-                features.append(
-                    i3d.extract_features(ip)
-                    .squeeze(0)
-                    .permute(1, 2, 3, 0)
-                    .data.cpu()
-                    .numpy()
-                )
-            np.save(os.path.join(save_dir, name[0]), np.concatenate(features, axis=0))
+            # features = []
+            # for start in range(1, t - 56, 1600):
+            #     end = min(t - 1, start + 1600 + 56)
+            #     start = max(1, start - 48)
+            #     ip = Variable(
+            #         torch.from_numpy(inputs.numpy()[:, :, start:end]).cuda(),
+            #         volatile=True,
+            #     )
+            #     features.append(
+            #         i3d.extract_features(ip)
+            #         .squeeze(0)
+            #         .permute(1, 2, 3, 0)
+            #         .data.cpu()
+            #         .numpy()
+            #     )
+            # np.save(os.path.join(save_dir, name[0]), np.concatenate(features, axis=0))
             print("Oh no, what happened?")
         else:
             with torch.no_grad():